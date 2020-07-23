a1a307a5c3cbddcad53e318b0962d28a0880edaf
Ty <68357354+jsxa@users.noreply.github.com>
2020-07-22 23:58:15 -0400

    Create ban-check.js

---
+++ ban-check.js
@@ -0,0 +1,49 @@
+const aa = require('discord.js');
+module.exports={
+    name: 'ban-check',
+    description: 'Checks bans',
+    category: 'info',
+    run: async(bot,message,args)=>{
+        message.channel.send('Logs')
+        message.channel.send('Powermammal2564 > CommandBlockExploit > Pban [ Ban date: 06/06/2020 ]')
+        message.channel.send('WaterDragon2k12 > Ban Evasion: Alt of Eboy Salem > Pbanned [ Ban date: 21/06/2020 ✓ ]')
+        message.channel.send('darknes2555 >  32k usage, 32k possesion, Malicious hacks (hacking in items) > Pbanned [ Ban date: 21/06/2020 ✓ ]')
+        message.channel.send('Mr AlexRex >  Nuker > Pbanned [ Ban date: 21/06/2020 ✓ ]')
+        message.channel.send('CosySugar909991 >  Nuker > Pbanned [ Ban date: 21/06/2020 ✓ ]')
+        message.channel.send('Devilstar08 > Malicious Hacks > Pbanned [Ban date: 22/06/2020 ✓]')
+        message.channel.send('Diablo > Hacked client usage, Noclip > pban [Ban date: 22/06/2020 ]')
+        message.channel.send('your boy jon819 > Spamming, 32k usage > sban [Ban date: 26/06/2020]')
+        message.channel.send('hackergamecraf > Heavy Griefing > seasonban (til 16S3) [Ban date: 28/06/2020]')
+        message.channel.send('thales400eb20 > Not enough gamerscore, Potential alt account > seasonban (til 16S3) [Ban date: 28/06/2020]')
+        message.channel.send('TTVbrumms24 > Slight griefing > 5 minute ban <Unbanned>')
+        message.channel.send('jafa3 > Griefing, alt > pban [Ban date: 28/06/2020]')
+        message.channel.send('ben10gamer775 > Using a leaked code, Alt account > pban [28/06/2020]')
+        message.channel.send('ISupLOVEYOU3000 > Spawnkilling > 5 min ban [28/06/2020]')
+        message.channel.send('ISupLOVEYOU3000 > Annoying the community alot, Griefing, Spawnkilling > seasonbanned [28/06/2020]')
+        message.channel.send('ben10gamer775 > Banned in a ban spree because not in discord > Unbanned ✓')
+        message.channel.send('IcyCcaleb > 32k usage, in the old map > pban')
+        message.channel.send('Sillymrc2 > Spamming/Advertising > pban [01/07/2020]')
+        message.channel.send('B Rich77 > Griefing > Tempbanned for 12 hours [01/07/2020]')
+        message.channel.send('TraupeDude > Rude to admins, trying to get an admin fired for no reason > 1 hour ban [02/07/2020]')
+        message.channel.send('sHaD kHatri > Crashing realm, Malicious hacks > pban {Priority:Major}')
+        message.channel.send('xForcedYT > Killaura > seasonban')
+        message.channel.send('your boy jon818 > Annoying the community, spawnkilling > soft pban (Locked in Gamemode Spectator)')
+        message.channel.send('your boy jon819 > Harrasing admins > pban{Priority:High}')
+        message.channel.send('Rith Gaming5317 > Literally admitting hes using a hacked client > 10/07/2020 {STARLIUM}')
+        message.channel.send('HASSAN IS KING > Spawnkilling > 11/07/2020 {STARLIUM}, AlphaAmirax > XP Exploit > 11/07/2020 {STARLIUM}')
+        message.channel.send('Clev3rName > Griefing > 12/07/2020 {STARLIUM}')
+        message.channel.send('(BigBoiHengleap) > Gamemode Creative Exploit (Most likely CBE) > Permbanned')
+        message.channel.send('sharkygaming23 > Malicious Hacks > Pban > 12/07/2020 {STARLIUM}')
+        message.channel.send('(AlecTheCrafter) > Malicious Hacks > Pban > 16/07/2020 {STARLIUM}')
+        message.channel.send('Gamergamer10914 > Malicious hacks > Pban > 16/07/2020 {STARLIUM}')
+        message.channel.send('Metro > Duping > 1 week ban > 16/07/2020')
+        message.channel.send('POKEsteve7309>enchant hacks>pban>16/07/2020')
+        message.channel.send('BrinerSalmon > malicious hacks > pban')
+        message.channel.send('HatchedLemur>32k usage>pban')
+        message.channel.send('kim7437>nuker,griefing>pban')
+        message.channel.send('Gifzn>toolbox/Admin abuse>pban')
+        message.channel.send('Done, made by tyler!')
+        message.channel.send('You can view the source code over on my github!(its just message.channel.send though)')
+    }
+}
+// have fun lmao
